7d95ffbb9b6fe610a9935f6f1dfca6ee7731df72
snvice <131165228+snvice@users.noreply.github.com>
2024-07-12 04:53:15 +0300

    change db to online version

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine  # to open and use db
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://u_4771ebc8_433a_4d58_96f9_c0e210c43d80:2n6647V6bki5Cv63lVXjVEJQu5TsxDVjaP9R664X61kZ6A8g1q5U@pg.rapidapp.io:5433/db_4771ebc8_433a_4d58_96f9_c0e210c43d80?ssl=true&application_name=rapidapp_nodejs"
 
 
 # SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:vice@localhost/TodoApplicationDatabase'  # used to create a location for the db in the fastapi app
