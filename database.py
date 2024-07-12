0836d7b3a234cd2bacb75ee6d2df2a483344fa48
snvice <131165228+snvice@users.noreply.github.com>
2024-07-12 04:36:11 +0300

    change db

--- database.py
+++ database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine  # to open and use db
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:vice@localhost/TodoApplicationDatabase'  # used to create a location for the db in the fastapi app
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+
+
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:vice@localhost/TodoApplicationDatabase'  # used to create a location for the db in the fastapi app
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
